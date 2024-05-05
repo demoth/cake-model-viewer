6acd57bc5215d4f2c699fe304df0976a5b139c76
demoth <bubnov.d.e@gmail.com>
2024-05-05 17:45:41 +0200

    WIP: md2 loading into mesh

--- core/src/main/kotlin/org/demoth/cake/CakeModelViewer.kt
+++ core/src/main/kotlin/org/demoth/cake/CakeModelViewer.kt
@@ -3,15 +3,21 @@ package org.demoth.cake
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.*
+import com.badlogic.gdx.graphics.GL20.GL_LINE_STRIP
+import com.badlogic.gdx.graphics.GL20.GL_TRIANGLES
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g3d.*
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
 import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader
 import com.badlogic.gdx.graphics.g3d.loader.ObjLoader
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
+import com.badlogic.gdx.graphics.g3d.utils.MeshBuilder
+import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder
 import com.badlogic.gdx.utils.UBJsonReader
 import ktx.graphics.use
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 
 /** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms.  */
@@ -36,9 +42,10 @@ class CakeModelViewer : ApplicationAdapter() {
         Gdx.input.inputProcessor = CameraInputController(camera)
         modelBatch = ModelBatch()
         models = mutableListOf(
-            ModelInstance(createModel()),
-            loadObjModel(),
-            loadG3dModel()
+//            ModelInstance(createModel()),
+//            loadObjModel(),
+//            loadG3dModel(),
+            loadMd2Model() // fixme: nothing is rendered.. :(
         )
 
         environment = Environment()
@@ -78,6 +85,87 @@ class CakeModelViewer : ApplicationAdapter() {
         crateInstance.transform.scale(0.01f, 0.01f, 0.01f)
         return crateInstance
     }
+
+    private fun loadMd2Model(): ModelInstance {
+        val md2Model: Md2Model = readMd2Model()
+
+        // put all vertices of the 1st frame into the buffer
+        val frame = md2Model.frames.first()!!
+        val vertexBuffer = frame.points.flatMap { listOf(it.x * frame.scale[0], it.y * frame.scale[1], it.z * frame.scale[2]) }.toFloatArray()
+
+        val vertexIndices: ShortArray = createVertexIndices(md2Model, frame)
+
+        val meshBuilder = MeshBuilder()
+        meshBuilder.begin(VertexAttributes(VertexAttribute.Position()), GL_TRIANGLES)
+        meshBuilder.addMesh(vertexBuffer, vertexIndices)
+        val mesh = meshBuilder.end()
+        // create a model from the created mesh
+        val model = Model()
+        model.materials.add(Material(ColorAttribute.createEmissive(Color.GREEN)))
+        model.meshes.add(mesh)
+        val modelInstance = ModelInstance(model)
+        modelInstance.transform.translate(2f, 0f, 0f)
+        modelInstance.transform.scale(0.05f, 0.05f, 0.05f)
+        return modelInstance
+    }
+
+    private fun createVertexIndices(model: Md2Model, frame: Md2Frame): ShortArray {
+        var glCmdIndex = 0 // todo: use queue to pop elements instead of using mutable index?
+        val result = mutableListOf<Short>()
+        while (true) {
+            val numOfPoints = model.glCmds[glCmdIndex]
+            glCmdIndex++
+            glCmdIndex += if (numOfPoints == 0) {
+                break
+            } else if (numOfPoints >= 0) {
+                // triangle strip
+                val vertices = mutableListOf<Int>()
+                for (i in glCmdIndex until (glCmdIndex + numOfPoints * 3) step 3) {
+                    vertices.add(model.glCmds[i + 2])
+                }
+                // converting strips into separate triangles
+                vertices.windowed(3).forEach {
+                    result.add(it[0].toShort())
+                    result.add(it[1].toShort())
+                    result.add(it[2].toShort())
+                }
+                numOfPoints * 3
+            } else {
+                // triangle fan
+                val vertices = mutableListOf<Int>()
+                for (i in glCmdIndex until (glCmdIndex - numOfPoints * 3) step 3) {
+                    vertices.add(model.glCmds[i + 2])
+                }
+                convertStripToTriangles(vertices).windowed(3).forEach {
+                    result.add(it[0].toShort())
+                    result.add(it[1].toShort())
+                    result.add(it[2].toShort())
+                }
+                (-numOfPoints * 3)
+            }
+
+        }
+        return result.toShortArray()
+    }
+
+    private fun convertStripToTriangles(vertices: List<Int>): List<Int> {
+        val result = mutableListOf<Int>()
+        vertices.drop(1).windowed(2).forEach {
+            result.add(vertices.first())
+            result.add(it.first())
+            result.add(it.last())
+        }
+        return result
+    }
+
+
+    fun readMd2Model(): Md2Model {
+        val byteBuffer = ByteBuffer
+            .wrap(Gdx.files.internal("adrenaline-model.md2").readBytes())
+            .order(ByteOrder.LITTLE_ENDIAN)
+        return Md2Model(byteBuffer, "her")
+    }
+
 
     override fun render() {
         Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f)
---
+++ core/src/main/kotlin/org/demoth/cake/Md2Format.kt
@@ -0,0 +1,158 @@
+package org.demoth.cake
+
+import java.nio.ByteBuffer
+import java.nio.FloatBuffer
+import java.nio.IntBuffer
+
+/**
+ * daliasframe_t
+ * A frame in the MD2 model
+ */
+
+data class Point(val x: Int, val y: Int, val z: Int)
+
+class Md2Frame(buffer: ByteBuffer, num_xyz: Int) {
+    var scale = floatArrayOf(0f, 0f, 0f) // multiply byte verts by this
+    var translate = floatArrayOf(0f, 0f, 0f) // then add this
+    var name // frame name from grabbing (size 16)
+            : String
+    var verts: IntArray
+    val points: Array<Point>
+
+    init {
+        scale[0] = buffer.float
+        scale[1] = buffer.float
+        scale[2] = buffer.float
+        translate[0] = buffer.float
+        translate[1] = buffer.float
+        translate[2] = buffer.float
+        val nameBuf = ByteArray(16)
+        buffer[nameBuf]
+        name = String(nameBuf).trim { it <= ' ' }
+
+        // vertices are all 8 bit, so no swapping needed
+        verts = IntArray(num_xyz)
+        points = Array(num_xyz) { Point(0, 0, 0) }
+
+
+        for (k in 0 until num_xyz) {
+            verts[k] = buffer.int
+            points[k] = Point(
+                verts[k] ushr 0 and 0xFF,
+                verts[k] ushr 8 and 0xFF,
+                verts[k] ushr 16 and 0xFF,
+            )
+        }
+    }
+}
+
+/**
+ * MD2 (quake2) model (previously named dmdl_t)
+ *
+ *
+ * Also known as "alias model" in the code - named after the model editor originally used (PowerAnimator)
+ *
+ *
+ * Header section contains size information (how many vertices, frames, skins, etc)
+ * and byte offsets to the respective sections.
+ * Body section are the data arrays.
+ *
+ *
+ * The GL commands (glcmd) format:
+ *
+ *  *  positive integer starts a tristrip command, followed by that many vertex structures.
+ *  *  negative integer starts a trifan command, followed by -x vertexes
+ *  *  zero indicates the end of the command list.
+ *
+ *
+ *
+ * A vertex consists of a floating point s, a floating point and an integer vertex index.
+ */
+class Md2Model(b: ByteBuffer) {
+    // Header
+    var ident: Int
+    var version: Int
+    var num_skins: Int
+    var num_vertices: Int
+    var num_tris: Int
+    var num_glcmds: Int // dwords in strip/fan command list
+
+    var num_frames: Int
+
+    // each skin is a MAX_SKINNAME string
+    var skinsOffset: Int
+    var firstFrameOffset: Int
+    var glCommandsOffset: Int
+    var endOfFileOffset: Int // end of file
+
+
+    // Body
+    var skinNames: Array<String?> = arrayOf()
+    var glCmds: IntArray = intArrayOf()
+
+    // all frames have vertex array of equal size (num_xyz)
+    var frames: Array<Md2Frame?> = arrayOf()
+
+    constructor(b: ByteBuffer, modelName: String) : this(b) {
+        loadBody(b)
+    }
+
+    /**
+     * Reads md2 header
+     */
+    init {
+        ident = b.int
+        version = b.int
+        b.int // skinwidth
+        b.int // skinheight
+        b.int // framesize: byte size of each frame
+        num_skins = b.int
+        num_vertices = b.int
+        b.int //num_st: greater than num_xyz for seams, parsed into dstvert_t
+        num_tris = b.int
+        num_glcmds = b.int // dwords in strip/fan command list
+        num_frames = b.int
+        skinsOffset = b.int // each skin is a MAX_SKINNAME string
+        b.int // offset from start until dstvert_t[]
+        b.int // offset from start until dtriangles[]
+        firstFrameOffset = b.int // offset for first frame
+        glCommandsOffset = b.int
+        endOfFileOffset = b.int // end of file
+    }
+
+    /**
+     * Loads the bulk of the data
+     */
+    private fun loadBody(buffer: ByteBuffer) {
+        //
+        //	   load the frames
+        //
+        frames = arrayOfNulls(num_frames)
+        buffer.position(firstFrameOffset)
+        for (i in 0 until num_frames) {
+            frames[i] = Md2Frame(buffer, num_vertices)
+        }
+
+        //
+        // load the glcmds
+        // STRIP or FAN
+        glCmds = IntArray(num_glcmds)
+        buffer.position(glCommandsOffset)
+        for (i in 0 until num_glcmds) {
+            glCmds[i] = buffer.int
+        }
+        skinNames = arrayOfNulls(num_skins)
+        val nameBuf = ByteArray(64)
+        buffer.position(skinsOffset)
+        for (i in 0 until num_skins) {
+            buffer[nameBuf]
+            skinNames[i] = String(nameBuf)
+            val n = skinNames[i]!!.indexOf('\u0000')
+            if (n > -1) {
+                skinNames[i] = skinNames[i]!!.substring(0, n)
+            }
+        }
+    }
+
+}
+
